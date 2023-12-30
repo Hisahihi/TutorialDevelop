bd8a605be621e946dbd8a2cc75a586b0870185c2
HIsahihi <jiuqingshui47@gmail.com>
2023-12-30 10:22:40 +0900

    1接続情報設定　２初期化スプリクト（SQL）の作成　３エンティティにUserファイル作成

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user(name, gender, age, email)
+     VALUES ("ちたお", "男性", 27, 'chitao.kirameki@mail.com');
+INSERT INTO user(name, gender, age, email)
+     VALUES ("たねぞう", "男性", 22, 'tanezo.kirameki@mail.com');
+INSERT INTO user(name, gender, age, email)
+     VALUES ("花子", "女性", 25, 'hanako.kirameki@mail.com');
